b792783b435c1328821f981478f8a4a9be9a0e96
pedro-git-projects <pedro.generic.email@gmail.com>
2022-05-13 15:04:24 -0300

    estrdada finalizada

--- estrada.js
+++ estrada.js
@@ -7,9 +7,20 @@ class Estrada {
 		this.esquerda = x - largura/2
 		this.direita = x + largura/2
 
-		const limite = 1000000 
+		const limite = 1000000 // Limite é um número arbitrário para o fim da estrada 
 		this.topo = -limite
 		this.final = limite 
+
+		const topoEsquerda = { x: this.esquerda, y: this.topo }
+		const topoDireita = { x: this.direita, y: this.topo }
+		const fimEsquerda = { x: this.esquerda, y: this.final }
+		const fimDireita = { x: this.direita, y: this.final }
+
+		/* Limites representa as possíveis hitboxes da estrada */
+		this.limites = [
+			[topoEsquerda, fimEsquerda],
+			[topoDireita, fimDireita],
+		]
 	}
 
 	/* 
@@ -32,24 +43,27 @@ class Estrada {
 		 	Usa interpolação linear para desenhar as faixas 
 			de acordo com numFaixas
 		*/
-		for(let i = 0; i <= this.numFaixas; i++) {
+		for(let i = 1; i<=this.numFaixas - 1; i++) {
 			const x = interpolacaoLinear(
 				this.esquerda,
 				this.direita,
 				i/this.numFaixas
 			)
 
-			/* Fazendo com que as faixas do centro fiquem pontilhadas */
-			if(i > 0 && i < this.numFaixas) {
-				ctx.setLineDash([20, 20])
-			} else {
-				ctx.setLineDash([])
-			}
-
+			ctx.setLineDash([20, 20]) // linhas pontilhadas
 			ctx.beginPath()
 			ctx.moveTo(x, this.topo)
 			ctx.lineTo(x, this.final)
 			ctx.stroke()
 		}
+
+		/* Desenhando as linhas das bordas de acordo com os limites de colisão */
+		ctx.setLineDash([])
+		this.limites.forEach(limite => {
+			ctx.beginPath()
+			ctx.moveTo(limite[0].x, limite[0].y)
+			ctx.lineTo(limite[1].x, limite[1].y)
+			ctx.stroke()
+		})
 	}
 }
